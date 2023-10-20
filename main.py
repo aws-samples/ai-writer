d2c26294993be731e2c4a7683f6360f25e1f4ec5
Shing Lyu <shinglyu@amazon.nl>
2023-10-20 13:18:25 +0000

    Use bedrock to generate article

--- main.py
+++ main.py
@@ -3,27 +3,66 @@
 
 
 import streamlit as st
-import pandas as pd
-import numpy as np
+import sys
+import boto3
+import json
 
+def invoke_llm(user_prompt):
+    client = boto3.client('bedrock-runtime', region_name='us-east-1')
+    prompt = f"""
+
+Human: Write {user_prompt}
+
+Assistant:"""
+    
+    print(prompt)
+    try:
+        response = client.invoke_model(
+            body = bytes(json.dumps({
+                "prompt": prompt,
+                "max_tokens_to_sample": 2048,
+                "temperature": 1.0,
+                "top_k": 250,
+                "top_p": 1,
+                "stop_sequences": [
+                "\\n\\nHuman:"
+                ],
+                "anthropic_version": "bedrock-2023-05-31"
+            }), 'utf-8'),
+            #modelId = "anthropic.claude-v2",
+            modelId = "anthropic.claude-instant-v1",
+            contentType = "application/json",
+            accept = "application/json"
+        )
+        response_text = response['body'].read().decode('utf8').strip()
+        response_json = json.loads(response_text)
+        completion = response_json['completion']
+        return completion
+    except Exception as e:
+        print('Error invoking endpoint')
+        print(e)
+        raise Exception("Error invoking LLM")
+        
 
 if "article" not in st.session_state: 
-    st.session_state["article"] = [
-    "This is the first paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph",
-    "This is the second paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is",
-    "This is the third paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text paragraph. It is a text" 
-    ]
+    st.session_state["article"] = []
 
 if "editing_idx" not in st.session_state: 
     st.session_state["editing_idx"] = None
 
 st.title("AI Writer")
 
-def update_paragraph(new_value):
-    print(editing_text_area)
-    print(new_value)
-    st.session_state["article"][idx] = editing_text_area
-    st.session_state["editing_idx"] = None
+article_prompt = st.text_area("What would you like to write?", placeholder="A short story about an unicorn")
+if st.button("Write"):
+    # Use langchain to invoke a Bedrock model to generate text based on article_prompt
+    # Split the text into paragraphs and add it to session_state["article"]
+    raw_article = invoke_llm(article_prompt)
+    st.session_state["article"] = raw_article.split("\n\n")
+    
+    
+
+# Add a devider
+st.markdown("---")
 
 for idx, paragraph in enumerate(st.session_state["article"]):
     if idx == st.session_state["editing_idx"]:
@@ -41,3 +80,7 @@ for idx, paragraph in enumerate(st.session_state["article"]):
         if st.button("^ edit", key=f"edit-{idx}"):
             st.session_state["editing_idx"] = idx
             st.rerun()
+            
+st.markdown('---')
+if st.button("Export finished article"):
+    st.text_area("Finished article", value="\n\n".join(st.session_state["article"]))
