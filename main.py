f24c09f7e229fe3ab299c440879272e10a075f40
Shing Lyu <shinglyu@amazon.nl>
2023-11-03 12:05:51 +0100

    fix: avoid calling LLM with empty prompt

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ def split_paragraphs(full_article):
         elif paragraph.strip() != "":
             output.append(paragraph)
     return output
-            
 
 if "article" not in st.session_state: 
     st.session_state["article"] = []
@@ -59,69 +58,84 @@ if "article" not in st.session_state:
 if "editing_idx" not in st.session_state: 
     st.session_state["editing_idx"] = None
 
-# Start of layout
-st.title("AI Writer")
+def main():
+    st.title("AI Writer")
 
-is_starting_over = len(st.session_state["article"]) == 0
+    is_starting_over = len(st.session_state["article"]) == 0
 
-with st.expander("Start over", expanded = is_starting_over):
-    article_prompt = st.text_area("What would you like to write?", placeholder="A short story about an unicorn")
-    
-    if st.button("Write"):
-        # Use langchain to invoke a Bedrock model to generate text based on article_prompt
-        # Split the text into paragraphs and add it to session_state["article"]
-        raw_article = invoke_llm(writing_prompt(article_prompt))
-        st.session_state["article"] = split_paragraphs(raw_article)
-        st.rerun()
+    with st.expander("Start over", expanded = is_starting_over):
+        article_prompt = st.text_area("What would you like to write?", placeholder="A short story about an unicorn")
         
-with st.expander("Revise the whole article", expanded = not is_starting_over):
-    overall_revise_instruction = st.text_area("How would you like to revise the whole article?", placeholder="Change from third-person to first-person")
-    if st.button("Revise"):
-        revised_article = invoke_llm(overall_revise_prompt(overall_revise_instruction, "\n\n".join(st.session_state["article"])))
-        st.session_state["article"] = split_paragraphs(revised_article)
-    
+        if st.button("Write"):
+            # Using this instead of using button.disabled because when you enter something into the text_area, 
+            # you need to unfocus the text_area before the button's state updates. This is bad UX.
+            if article_prompt.strip() == "": 
+                st.error("Please enter a prompt")
+                return
+            raw_article = invoke_llm(writing_prompt(article_prompt))
+            st.session_state["article"] = split_paragraphs(raw_article)
+            st.rerun()
+            
+    with st.expander("Revise the whole article", expanded = not is_starting_over):
+        overall_revise_instruction = st.text_area("How would you like to revise the whole article?", placeholder="Change from third-person to first-person")
+        if st.button("Revise"):
+            if st.session_state["article"] == []:
+                st.error("Please write something first")
+                return
+            if overall_revise_instruction.strip() == "": 
+                st.error("Please enter a prompt")
+                return
+            revised_article = invoke_llm(overall_revise_prompt(overall_revise_instruction, "\n\n".join(st.session_state["article"])))
+            st.session_state["article"] = split_paragraphs(revised_article)
+        
 
-# Add a devider
-st.markdown("---")
+    # Add a devider
+    st.markdown("---")
 
-for idx, paragraph in enumerate(st.session_state["article"]):
-    if idx == st.session_state["editing_idx"]:
-        st.markdown("---")
-        text_col, action_col = st.columns([0.8, 0.2])
-        with text_col:
-            editing_text_area = st.text_area("(Editing)", 
-                value=paragraph, 
-                #on_change=update_paragraph
-            )
-        with action_col:
-            if editing_text_area != st.session_state["article"][idx] or st.button("Save", key=f"edit-{idx}"):
-                st.session_state["article"][idx] = editing_text_area
-                st.session_state["editing_idx"] = None
-                st.session_state["article"] = split_paragraphs("\n".join(st.session_state["article"])) # One paragraph might be edited to two
-                st.rerun()
-            revise_instruction= st.text_area("How would you like to revise this paragraph?", placeholder="Make the tone softer")
-            if st.button("Revise", key=f"revise-{idx}"):
-                revised_paragraph = invoke_llm(revise_prompt(revise_instruction, editing_text_area))
-                st.session_state["article"][idx] = revised_paragraph
-                st.session_state["article"] = split_paragraphs("\n".join(st.session_state["article"])) # One paragraph might be edited to two
-                st.rerun()
-        st.markdown("---")
-            
-            
-    else:
-        text_col, action_col_1, action_col_2 = st.columns([0.8, 0.1, 0.12])
-        with text_col:
-            paragraph
-    # when the paragraph is clicked, turn the text into a text area with the paragraph as placeholder
-        with action_col_1:
-            if st.button("edit", key=f"edit-{idx}"):
-                st.session_state["editing_idx"] = idx
-                st.rerun()
-        with action_col_2:
-            if st.button("Delete", key=f"delete-{idx}"):
-                st.session_state["article"].pop(idx)
-                st.rerun()
-            
-st.markdown('---')
-st.markdown("## Copy the article below")
-st.text_area("Finished article", value="\n".join(st.session_state["article"]))
+    for idx, paragraph in enumerate(st.session_state["article"]):
+        if idx == st.session_state["editing_idx"]:
+            st.markdown("---")
+            text_col, action_col = st.columns([0.8, 0.2])
+            with text_col:
+                editing_text_area = st.text_area("(Editing)", 
+                    value=paragraph, 
+                    #on_change=update_paragraph
+                )
+            with action_col:
+                if editing_text_area != st.session_state["article"][idx] or st.button("Save", key=f"edit-{idx}"):
+                    st.session_state["article"][idx] = editing_text_area
+                    st.session_state["editing_idx"] = None
+                    st.session_state["article"] = split_paragraphs("\n".join(st.session_state["article"])) # One paragraph might be edited to two
+                    st.rerun()
+                revise_instruction= st.text_area("How would you like to revise this paragraph?", placeholder="Make the tone softer")
+                if st.button("Revise", key=f"revise-{idx}"):
+                    if revise_instruction.strip() == "": 
+                        st.error("Please enter a prompt")
+                        return
+                    revised_paragraph = invoke_llm(revise_prompt(revise_instruction, editing_text_area))
+                    st.session_state["article"][idx] = revised_paragraph
+                    st.session_state["article"] = split_paragraphs("\n".join(st.session_state["article"])) # One paragraph might be edited to two
+                    st.rerun()
+            st.markdown("---")
+                
+                
+        else:
+            text_col, action_col_1, action_col_2 = st.columns([0.8, 0.1, 0.12])
+            with text_col:
+                paragraph
+        # when the paragraph is clicked, turn the text into a text area with the paragraph as placeholder
+            with action_col_1:
+                if st.button("edit", key=f"edit-{idx}"):
+                    st.session_state["editing_idx"] = idx
+                    st.rerun()
+            with action_col_2:
+                if st.button("Delete", key=f"delete-{idx}"):
+                    st.session_state["article"].pop(idx)
+                    st.rerun()
+                
+    st.markdown('---')
+    st.markdown("## Copy the article below")
+    st.text_area("Finished article", value="\n".join(st.session_state["article"]))
+
+if __name__ == "__main__":
+    main()
